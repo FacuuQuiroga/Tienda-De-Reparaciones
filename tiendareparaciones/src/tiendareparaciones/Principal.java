d78f183d730d0333fd73be5d9c2f4a96c3798b60
Facu <Facu@DESKTOP-H2N0U7I>
2021-10-10 22:25:44 +0200

    holo

--- tiendareparaciones/src/tiendareparaciones/Principal.java
+++ tiendareparaciones/src/tiendareparaciones/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.printf("holas q tales");
+		System.out.printf("holas q tssales");
 	}
 
 }
