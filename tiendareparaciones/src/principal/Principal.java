6bddbabd6b05076d0f1eac36f9b4ee4ba8fe6553
FacuuQuiroga <facundoqc56@educastur.es>
2021-10-21 18:41:34 +0200

    prueba

--- tiendareparaciones/src/principal/Principal.java
+++ tiendareparaciones/src/principal/Principal.java
@@ -5,6 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.printf("holas q tssales");
+		System.out.printf("holas q tssales");
+
 	}
 
 }
