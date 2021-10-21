939b0871bfd48ff75b69ceafb06dbc13a58df5a1
alumnado <alumnado@A204PC17.desarrolloweb.local>
2021-10-21 19:12:44 +0200

    prueba nico

--- tiendareparaciones/src/principal/Principal.java
+++ tiendareparaciones/src/principal/Principal.java
@@ -4,8 +4,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.printf("holas q tssales");
-		System.out.printf("holas q tssales");
+		System.out.println ("holas q tssales");
+		System.out.println ("holas q tssales");
+		System.out.println ("askljdlaskjd");
 
 	}
 
