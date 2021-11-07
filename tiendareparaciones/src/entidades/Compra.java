6d37809b626d6f8cd550d9adfc07bdc369931f37
simon <simon@DESKTOP-D5A1IOV>
2021-11-07 15:05:18 +0100

    mi parte

--- tiendareparaciones/src/entidades/Compra.java
+++ tiendareparaciones/src/entidades/Compra.java
@@ -3,8 +3,48 @@ package entidades;
 
 public class Compra {
 
-	public Compra() {
-		// TODO Auto-generated constructor stub
+	private long idCompra = 0;
+	//idEquipo es el identificador del elemento compra
+	//valor entero > 0
+	//y valor por defecto 0
+	private char metodoDePago = 'A';
+	//metodoDePago expresa la modalidad de dicho pago
+	//se expresa en caracteres
+	protected double precioTotal = 0;
+	//indica el computo total del precio de equipo
+	//expresado en numero reales con simbolos de puntuacion
+
+////Getters , setters and to string
+	public long getIdEquipo() {
+		return idEquipo;
+	}
+
+	public void setIdEquipo(long idEquipo) {
+		this.idEquipo = idEquipo;
+	}
+
+	public char getMetodoDePago() {
+		return metodoDePago;
+	}
+
+	public void setMetodoDePago(char metodoDePago) {
+		this.metodoDePago = metodoDePago;
+	}
+
+	public double getPrecioTotal() {
+		return precioTotal;
+	}
+
+	public void setPrecioTotal(double precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+
+	// to string
+	@Override
+	public String toString() {
+		return "Compra [idEquipo=" + idEquipo + ", metodoDePago=" + metodoDePago + ", precioTotal=" + precioTotal + "]";
 	}
 
 }
+
+
