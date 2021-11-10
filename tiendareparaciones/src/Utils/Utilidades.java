3df1e5b73d9f8e79a36dae70dacc640a727ce3ba
Facu <Facu@DESKTOP-H2N0U7I>
2021-11-10 19:18:36 +0100

    Creacion de clase Validador, y modificacion de la clase Utilidades

--- tiendareparaciones/src/Utils/Utilidades.java
+++ tiendareparaciones/src/Utils/Utilidades.java
@@ -1,10 +1,11 @@
 package Utils;
+
 import java.util.Scanner;
 
 public class Utilidades {
-	//para relacion clientes y servicios (facu)
+	// para relacion clientes y servicios (facu)
 	public static final int TAMCLIENTES = 100;
-	
-//hay q preguntar si se puede hacer algo asi para no tener que escribir en cada clase las variables de teclado
-//public static final Scanner TECLADO = new Scanner(System.in); 
+
+
+
 }
---
+++ tiendareparaciones/src/Utils/Validador.java
@@ -0,0 +1,23 @@
+
+package Utils;
+
+public class Validador {
+
+	// para validar las variables del cliente y empleado, interesa que sea publico y no
+	// privado para poder utilizarlo en otros lados del codigo
+	// Codificado por facu
+	public static boolean validarNombre(String nombre) {
+		
+		return false;
+	}
+
+	public static boolean validarNif(String nif) {
+
+		return false;
+	}
+	
+	public static boolean validarDireccion(String direccion) {
+
+		return false;
+	}
+}
