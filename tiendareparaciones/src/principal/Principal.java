7c31aabedd6b3448b23e49fa46260ed169b2ce8b
Facu <Facu@DESKTOP-H2N0U7I>
2021-11-12 18:57:03 +0100

    Cambio del bucle para el menu a un do While

--- tiendareparaciones/src/principal/Principal.java
+++ tiendareparaciones/src/principal/Principal.java
@@ -9,45 +9,54 @@ import entidades.Servicio;
 public class Principal {
 
 	public static void main(String[] args) {
-		Scanner teclado;/// IMPORTATE PAL TECLADO
-		teclado = new Scanner(System.in);/// IMPORTANTE PAL TECLADO
+		/* IMPORTATE PAL TECLADO */
+		Scanner teclado;
+		teclado = new Scanner(System.in);
 		//////////////////////////////////////////////////////////////
-//	
-//		System.out.println ("Programa de gestion de servicios de una tienda de reparaciones");
-//		System.out.println("Que desea hacer? \n");
-//		System.out.println("1- Registrar cliente");
-//		System.out.println("2- Registrar Servicio");
-//		System.out.println("3- Otros");
-//		System.out.println("9- Salir");
-//
-//		int auxMenu = teclado.nextInt();
-//		
-//		switch (auxMenu) {
-//			case 1:
-//				System.out.println("Registro del cliente: ");
-//				Cliente facu= Cliente.nuevoCliente();
-//				System.out.println(facu);
-//				
-//				break;
-//			case 2:
-//				System.out.println("Registro de nuevo Servicio");
-//				System.out.println("Que tipo de servicio es?");
-//				System.out.println("1-Compra");
-//				System.out.println("2-Mantenimiento");
-//				break;
-//			case 3:
-//				System.out.println("otros otros");
-//				
-//				break;
-//			case 4:
-//				System.out.println("Boton secreto activado, se procede a la explotacion del pc");
-//				System.out.println("PUM!");
-//				break;
-//			default:
-//				System.out.println("Introduce solo numeros entre 0 y 3");
-//				
-//		}
 
+		boolean salir = false;
+		int opcion; // Guardaremos la opción del usuario
+		do {
+			System.out.println("Programa de gestion de servicios de una tienda de reparaciones");
+			System.out.println("Que desea hacer? \n");
+			System.out.println("1- Registrar cliente");
+			System.out.println("2- Registrar Servicio");
+			System.out.println("3- Otros");
+			System.out.println("9- Salir");
+			opcion = teclado.nextInt();
+
+			switch (opcion) {
+			case 1:
+				/* Registro del cliente */
+				Cliente facu = Cliente.nuevoCliente();
+				System.out.println(facu);
+				return;
+			case 2:
+				/* Registro de nuevo Servicio */
+				System.out.println("Que tipo de servicio es?");
+				System.out.println("1-Compra");
+				System.out.println("2-Mantenimiento");
+				int auxServ = teclado.nextInt();
+				if (auxServ == 2) {
+					Servicio facu2 = Mantenimiento.nuevoServicio();
+					System.out.println(facu2);
+				}
+				return;
+			case 3:
+				System.out.println("otros otros");
+
+				return;
+			case 4:
+				System.out.println("Boton secreto activado, se procede a la explotacion del pc");
+				System.out.println("PUM!");
+				break;
+			case 9:
+				if (salir == false) {
+					salir = true;
+				}
+			default:
+				System.out.println("Guardando... nada que guardar,ta luego!");
+			}
+		} while (!salir);
 	}
-	
 }
