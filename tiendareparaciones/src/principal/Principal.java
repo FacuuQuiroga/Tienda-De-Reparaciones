598be9f01904a285cfbb5f12fafec086961eb245
Facu <Facu@DESKTOP-H2N0U7I>
2021-11-17 17:26:42 +0100

    Modificacion del do While del menu, con su validacion y su correcto funcionamiento

--- tiendareparaciones/src/principal/Principal.java
+++ tiendareparaciones/src/principal/Principal.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import entidades.Cliente;
 import entidades.Mantenimiento;
+import entidades.Reparacion;
 import entidades.Servicio;
 
 public class Principal {
@@ -12,51 +13,60 @@ public class Principal {
 		/* IMPORTATE PAL TECLADO */
 		Scanner teclado;
 		teclado = new Scanner(System.in);
-		//////////////////////////////////////////////////////////////
+		System.out.println("Programa de gestion de servicios de una tienda de reparaciones");
+		/* Guardaremos la opción del usuario */
+		int menu = 0;
+		/* para validar la opcion introducida */
+		boolean menuValido = false;
 
-		boolean salir = false;
-		int opcion; // Guardaremos la opción del usuario
 		do {
-			System.out.println("Programa de gestion de servicios de una tienda de reparaciones");
-			System.out.println("Que desea hacer? \n");
-			System.out.println("1- Registrar cliente");
-			System.out.println("2- Registrar Servicio");
-			System.out.println("3- Otros");
-			System.out.println("9- Salir");
-			opcion = teclado.nextInt();
+			mostrarMenuPrincipal();
+			menu = teclado.nextInt();
+			/*
+			 * Para validar que lo que introduzca el usuiario sea una opcion correcta. Es un
+			 * if y else comprimido, con un operador ternario, si el operador ternario es
+			 * correcto, se ejecuta false, si no es correcto, devuelve true
+			 */
+			menuValido = (menu < 1 || menu > 5 ? false : true);
 
-			switch (opcion) {
+			if (!menuValido) {
+				System.out.println("Opcion Incorrecta");
+			}
+
+			switch (menu) {
 			case 1:
 				/* Registro del cliente */
 				Cliente facu = Cliente.nuevoCliente();
 				System.out.println(facu);
-				return;
-			case 2:
+				break;
+			case 3:
 				/* Registro de nuevo Servicio */
 				System.out.println("Que tipo de servicio es?");
 				System.out.println("1-Compra");
 				System.out.println("2-Mantenimiento");
 				int auxServ = teclado.nextInt();
-				if (auxServ == 2) {
-					Servicio facu2 = Mantenimiento.nuevoServicio();
-					System.out.println(facu2);
+				if (auxServ == 1) {
+					System.out.println("registrar una compra( a implementar)");
+				} else {
+					Servicio facuM = Mantenimiento.nuevoServicio();
+					System.out.println(facuM);
 				}
-				return;
-			case 3:
-				System.out.println("otros otros");
-
-				return;
-			case 4:
-				System.out.println("Boton secreto activado, se procede a la explotacion del pc");
-				System.out.println("PUM!");
 				break;
-			case 9:
-				if (salir == false) {
-					salir = true;
-				}
-			default:
-				System.out.println("Guardando... nada que guardar,ta luego!");
+			case 4:
+				System.out.println("registrar nueva reparacion ( a implementar)");
+				break;
 			}
-		} while (!salir);
+
+		} while (!menuValido);
+	}
+
+	public static void mostrarMenuPrincipal() {
+
+		System.out.println("Que desea hacer?" + "\n");
+		System.out.println("1- Registrar cliente");
+		System.out.println("2- Para ver Servicio");
+		System.out.println("3- Registrar Servicio");
+		System.out.println("4- Registrar nueva reparacion");
+		System.out.println("5- Salir");
 	}
 }
